389d11d4c6c2d1ce5f817361d9b6f9ba7be33cd8
Maryam Salimi <salimi.5134@gmail.com>
2025-07-26 14:50:17 +0330

    fix logo link

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,7 +2,7 @@ import { Speaker } from '../globals/Icons';
 
 export function Logo({ color, size, textStyle }) {
   return (
-    <a href='/s/iran'>
+    <a href='/'>
       <div className='flex gap-3 items-center'>
         <Speaker color={color} size={size} />
         <h2 className={textStyle}>شیپورچی</h2>
