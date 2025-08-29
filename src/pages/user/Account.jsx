a009d9f0a3f9eb5c5dd111d453ef0d3603f4fe4f
Maryam Salimi <salimi.5134@gmail.com>
2025-08-30 01:01:45 +0330

    ّFixed check user status

--- src/pages/user/Account.jsx
+++ src/pages/user/Account.jsx
@@ -1,36 +1,33 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Menu from '../../components/user/menu/Menu';
 import { updateUserStatus } from '../../services/user/updateUserStatus';
 import { useEffect } from 'react';
 // import { useEffect } from 'react';
 
 export default function Account() {
+  const location = useLocation();
   const baseURL = import.meta.env.VITE_BASE_URL;
+
   useEffect(() => {
+    // هر بار مسیر تغییر کرد
     updateUserStatus('online');
 
-    // هندل کردن بسته شدن پنجره یا رفرش
+    return () => {
+      // وقتی مسیر ترک شد
+      updateUserStatus('offline');
+    };
+  }, [location.pathname]);
+
+  useEffect(() => {
     const handleBeforeUnload = () => {
       const url = `${baseURL}/api/users/status/offline`;
-      const data = JSON.stringify({}); // اگر بک‌اند نیاز به body نداره خالی بذار
-
-      // استفاده از sendBeacon چون مطمئن‌تره
+      const data = JSON.stringify({});
       navigator.sendBeacon(url, data);
     };
 
-    // هندل کردن تغییر صفحه (رفتن به جای دیگه)
-    const handleVisibilityChange = () => {
-      if (document.visibilityState === 'hidden') {
-        updateUserStatus('offline');
-      }
-    };
-
     window.addEventListener('beforeunload', handleBeforeUnload);
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-
     return () => {
       window.removeEventListener('beforeunload', handleBeforeUnload);
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, []);
 
